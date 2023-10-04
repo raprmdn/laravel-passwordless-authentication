acc4ae4cce7577aeea77be5295c37394fbca4abe
raprmdn <rafiputraramadhan14@gmail.com>
2023-10-04 13:46:01 +0700

    Setup project.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return inertia('Index');
-});
+Route::get('/', \App\Http\Controllers\IndexController::class)
+    ->name('index')->middleware('guest');
